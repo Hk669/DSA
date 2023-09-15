55f568852283b07d1ff1340b987c19f214e2d02f
Hk669 <hrushi669@gmail.com>
2023-09-15 23:53:54 +0530

    tests

--- tests/search_algos_testing.py
+++ tests/search_algos_testing.py
@@ -1,13 +1,11 @@
 import unittest
-from algorithms.arrays import (
-    binarySearch
-)
+from algorithms.arrays import binarySearch
 
 class SearchingAlgosTest(unittest.TestCase):
     def test_binarysearch(self):
-        self.assertTrue(binarySearch([1,2,3,4,5,67,98,107],98),6)
+        self.assertEqual(binarySearch([1,2,3,4,5,67,98,107],98),6)
 
-        self.assertTrue(binarySearch([23,45,64,73,87,89,98],23),0)
+        self.assertEqual(binarySearch([23,45,64,73,87,89,98],23),0)
 
         self.assertEqual(binarySearch([1, 2, 3, 4, 5], 6), -1)
 
--- tests/twoSum_testing.py
+++ tests/twoSum_testing.py
@@ -6,13 +6,13 @@ from algorithms.arrays import (
 class TwoSumTesting(unittest.TestCase):
 
     def testingTwosum(self):
-        self.assertTrue(twoSum([23,45,10,12,34],22),[2,3])
+        self.assertEqual(twoSum([23,45,10,12,34],22),[2,3])
 
-        self.assertTrue(twoSum([1,2,3,4,5,6,7,8,9,10],3),[0,1])
+        self.assertEqual(twoSum([1,2,3,4,5,6,7,8,9,10],3),[0,1])
 
         nums = [2,3,4,5,6]
         target = 11
-        self.assertTrue(twoSum(nums,target),[3,4])
+        self.assertEqual(twoSum(nums,target),[3,4])
 
 if __name__ == '__main__':
     unittest.main()
