cf101e3a359ac20797ed1502f2e01fe077ed2c29
Hk669 <hrushi669@gmail.com>
2023-10-28 23:31:02 +0530

    107

---
+++ algorithms/binarytree/107.py
@@ -0,0 +1,37 @@
+from collections import deque
+
+class TreeNode(object):
+    def __init__(self, val=0, left=None, right=None):
+        self.val = val
+        self.left = left
+        self.right = right
+
+
+class Solution(object):
+    def levelOrderBottom(self, root):
+        """
+        :type root: TreeNode
+        :rtype: List[List[int]]
+        """
+        if not root:
+            return []
+
+        result = []
+        q = deque()
+        q.append(root)
+
+        while q:
+            qlen = len(q)
+            level = []
+            for i in range(qlen):
+                node = q.popleft()
+
+                if node:
+                    level.append(node.val)
+                    q.append(node.left)
+                    q.append(node.right)
+
+            if level:
+                result.append(level)
+        
+        return reversed(result)
